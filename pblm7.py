f080bfecd3cca28d7dc0902b220f56d78f821e18
Rahul Indra <indrarahul2013@gmail.com>
2018-07-12 19:29:20 +0530

    Create pblm7.py

---
+++ pblm7.py
@@ -0,0 +1,15 @@
+def prime(n):
+	if n==2 or n==3: return 1
+	if n%2 == 0 or n<2: return 0
+	for i in range(3,int(n**0.5)+1,2):
+		if n%i == 0:
+			return 0
+	return 1
+
+j = 0
+for i in range(2,150000):
+	if(prime(i)):
+		j +=1
+	if j == 10001:
+		print(i)
+		break
